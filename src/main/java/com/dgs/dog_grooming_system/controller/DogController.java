ebcb2e7f330d2358d6e3fd84c4542e659efb2193
Hector Gonzalez <heco_gs@hotmail.com>
2024-10-07 12:05:36 -0600

    Update code

--- src/main/java/com/dgs/dog_grooming_system/controller/DogController.java
+++ src/main/java/com/dgs/dog_grooming_system/controller/DogController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
